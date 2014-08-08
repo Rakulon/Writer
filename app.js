36c9e371f590be094dfae2c873ec6b14dc678329
Rakulon <kekknyx@gmail.com>
2014-08-07 22:41:15 -0700

    a message for heroku

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ app.get('*', function(req,res){
 // 	})
 // })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("server listening on port 3000")
 })
 
